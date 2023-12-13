6505ab247e172aa27f94c94bfc2cf99c6d36b441
slb <belden.stephen@gmail.com>
2023-12-12 22:38:49 -0700

    add comments, mutability, functions with namespaces

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@
 // 2023-12-12
 
 fn main() {
+    // Unused intentionally; starts with underscore
     let _three = 3;
 
+    // Note the function called before the line on which it's declared
     println!("{}", value());
+
+    let name = "Lee";
+
+    // Mutability must be explicitly declared
+    let mut _name_length = name.len();
+
+    // Types are namespaces. Methods can be called like functions.
+    _name_length = str::len(name);
 }
 
+// Blocks are expressions, they return a value
 fn value() -> i32 {
-    3
+    4
 }
+
